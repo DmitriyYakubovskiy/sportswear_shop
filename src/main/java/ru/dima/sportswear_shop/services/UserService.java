733876c6b31ba76acfeb94d185e8f237f46d7dbb
DmitriyYakubovskiy <93999486+DmitriyYakubovskiy@users.noreply.github.com>
2025-03-14 21:38:51 +0500

    close

--- src/main/java/ru/dima/sportswear_shop/services/UserService.java
+++ src/main/java/ru/dima/sportswear_shop/services/UserService.java
@@ -2,10 +2,7 @@ package ru.dima.sportswear_shop.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.dima.sportswear_shop.entities.User;
 import ru.dima.sportswear_shop.repositories.UserRepository;
-
-import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,9 +11,5 @@ public class UserService {
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
-    }
-
-    public Optional<User> getUserByUsername(String username) {
-        return userRepository.findByUsername(username);
     }
 }
